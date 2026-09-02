33d1f166e0e116902e4bcab7c1e3f8b609f424de
dani e riri <danielle.7853579@aluno.mg.gov.br>
2026-09-02 14:34:31 -0300

    feat: concluir cadastro de candidatos

--- SistemaVotacao.java
+++ SistemaVotacao.java
@@ -102,6 +102,7 @@ public class SistemaVotacao {
                     if (numerosCandidatos[j] == numero) {
                         numeroRepetido = true;
                         break;
+                        
                     }
                 }
 
@@ -111,7 +112,23 @@ public class SistemaVotacao {
                 }
 
                 break;
+
             }
+                        String nome;
+
+            do {
+                System.out.print("Nome do candidato: ");
+                nome = scanner.nextLine().trim();
+
+                if (nome.isEmpty()) {
+                    System.out.println("O nome não pode ficar vazio.");
+                }
+            } while (nome.isEmpty());
+
+            numerosCandidatos[i] = numero;
+            nomesCandidatos[i] = nome;
+            votosCandidatos[i] = 0;
+            quantidadeCandidatos++;
         }
     }
 }
